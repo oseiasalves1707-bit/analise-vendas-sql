2f29ab7b48b391f3ae8da5959d0250d56096c487
Oseias Alves Pereira <oseiasalves1707@gmail.com>
2026-03-11 18:55:27 -0300

    Add files via upload

---
+++ Analisevendas.sql
@@ -0,0 +1,11 @@
+INSERT INTO vendas VALUES
+(6,'2025-02-01','Sofá Retrátil','Móveis',1,2500,'São Paulo'),
+(7,'2025-02-02','Smart TV 55','Eletrônicos',1,3200,'Guarulhos'),
+(8,'2025-02-02','Mesa de Jantar 6 Lugares','Móveis',1,1800,'Mogi das Cruzes'),
+(9,'2025-02-03','Geladeira Frost Free','Eletrodomésticos',1,4200,'Suzano'),
+(10,'2025-02-04','Notebook i5','Eletrônicos',2,3500,'São Paulo'),
+(16,'2025-02-05','Cadeira Gamer','Móveis',1,950,'Itaquaquecetuba'),
+(17,'2025-02-05','Micro-ondas','Eletrodomésticos',1,700,'Guarulhos'),
+(18,'2025-02-06','Guarda-roupa 3 Portas','Móveis',1,2100,'Mogi das Cruzes'),
+(19,'2025-02-07','Smartphone 128GB','Eletrônicos',3,1800,'São Paulo'),
+(20,'2025-02-08','Rack para TV','Móveis',1,650,'Suzano')
